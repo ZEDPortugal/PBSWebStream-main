aebb4b6a6ff9c70a15cc715da4e3799d0189f538
ZEDPortugal <123491971+ZEDPortugal@users.noreply.github.com>
2024-01-01 20:55:40 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,12 +30,17 @@ const backgroundImages = [
 export default function Home() {
   const [selectedSource, setSelectedSource] = useState(0);
   const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [audioPlayerKey, setAudioPlayerKey] = useState(0); // Add state for audio player key
 
   const handleSourceChange = (index: number) => {
     // Pause the audio player when source changes
     if (audioRef.current) {
       audioRef.current.pause();
     }
+
+    // Reset the state of DesignedAudioPlayer by changing the key
+    setAudioPlayerKey((prevKey) => prevKey + 1);
+
     setSelectedSource(index);
   };
 
@@ -51,11 +56,12 @@ export default function Home() {
     >
       <img className="absolute w-64 top-3 hover:w-72 ease-in-out duration-300" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Philippine_Broadcasting_Service_%28PBS%29.svg/270px-Philippine_Broadcasting_Service_%28PBS%29.svg.png" alt="" />
 
-      <div className='box fixed top-32 glass rounded-full shadow-lg h-96 w-96 hover:w-1/2 hover:drop-shadow-2xl hover:bg-gray-800 hover:bottom-36 ease-in-out duration-500 flex flex-col items-center'>
+      <div className='box fixed top-32 glass rounded-full shadow-lg h-96 w-96 hover:w-80 hover:drop-shadow-2xl hover:bg-gray-800 hover:top-28 ease-in-out duration-500 flex flex-col items-center'>
         <img className="absolute top-10 object-scale-down w-48 drop-shadow-2xl" src={sources[selectedSource].logo} alt="LOGO" />
         <div className="absolute bottom-10">
           <div>
-            <DesignedAudioPlayer src={sources[selectedSource].src}/>
+            {/* Add key prop to DesignedAudioPlayer */}
+            <DesignedAudioPlayer key={audioPlayerKey} src={sources[selectedSource].src}/>
           </div>
         </div>
       </div>
@@ -68,7 +74,7 @@ export default function Home() {
             onClick={() => handleSourceChange(index)}
           >
             <button
-              className="relative top-96 mt-36 glass hover:text-white focus:outline-none ease-in-out duration-300  text-gray-300 box mx-2 py-2 px-7 rounded-2xl hover:bg-gray-800 hover:text-xl ease-in-out duration-500 "
+              className="relative font-bold top-96 mt-36 glass hover:text-white focus:outline-none ease-in-out duration-300  text-gray-300 box mx-2 py-3 px-10 rounded-full hover:bg-gray-800 hover:text-xl ease-in-out duration-500 "
             >
               {index === 0 ? 'FM1' : index === 1 ? 'FM2' : 'RP1'}
             </button>
