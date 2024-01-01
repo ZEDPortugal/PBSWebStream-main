2bcce9da5e4de2b39a32194a0d3073c77722a6c3
ZEDPortugal <123491971+ZEDPortugal@users.noreply.github.com>
2024-01-01 20:56:00 +0800

    Update DesignedAudioPlayer.tsx

--- src/app/DesignedAudioPlayer.tsx
+++ src/app/DesignedAudioPlayer.tsx
@@ -12,6 +12,7 @@ type DesignedAudioPlayerProps = {
 const DesignedAudioPlayer: React.FC<DesignedAudioPlayerProps> = ({ src }) => {
   const audioRef = useRef<HTMLAudioElement | null>(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [volume, setVolume] = useState(1); // Initialize volume to maximum (1)
 
   useEffect(() => {
     const audio = audioRef.current;
@@ -49,11 +50,19 @@ const DesignedAudioPlayer: React.FC<DesignedAudioPlayerProps> = ({ src }) => {
     }
   };
 
+  const handleVolumeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newVolume = parseFloat(event.target.value);
+    setVolume(newVolume);
+    if (audioRef.current) {
+      audioRef.current.volume = newVolume;
+    }
+  };
+
   return (
     <div className="text-white feature flex items-center justify-center flex-col relative text-center mx-12">
       <button
         onClick={togglePlayPause}
-        className={` ease-in-out duration-500 text-3xl p-4 rounded-full ${
+        className={` text-3xl p-4 rounded-full ${
           isPlaying ? 'animate-pulse ease-in-out duration-500 bg-red-600 hover:bg-red-700' : ''
         } focus:outline-none focus:ring-2 ${
           isPlaying ? ' ease-in-out duration-500 focus:ring-red-700' : ''
@@ -61,6 +70,19 @@ const DesignedAudioPlayer: React.FC<DesignedAudioPlayerProps> = ({ src }) => {
       >
         {isPlaying ? <FaStop /> : <FaPlay />}
       </button>
+
+      <div className="mt-2 rounded-full">
+        <label className=" text-sm ease-in-out duration-300">
+        <input
+        
+          type="range"
+          min="0"
+          max="1"
+          step="0.01"
+          value={volume}
+          onChange={handleVolumeChange}
+        /></label>
+      </div>
 
       <audio ref={audioRef} src={src} preload="none" />
     </div>
